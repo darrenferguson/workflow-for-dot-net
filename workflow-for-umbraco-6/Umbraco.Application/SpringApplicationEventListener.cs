f3bff0826b87518b35f216af8ebd3361ece4260e
darrenferguson <df@darren-ferguson.com>
2013-11-11 13:30:38 +0000

    More v6 compatibility work

---
+++ workflow-for-umbraco-6/Umbraco.Application/SpringApplicationEventListener.cs
@@ -0,0 +1,25 @@
+﻿using FergusonMoriyam.Workflow.Interfaces.Application.Event;
+using Spring.Context;
+using Spring.Context.Events;
+
+namespace FergusonMoriyam.Workflow.Umbraco.Application
+{
+    public class SpringApplicationEventListener : IApplicationEventListener 
+    {
+
+        public void HandleApplicationEvent(object sender, ApplicationEventArgs e)
+        {
+            var eventArgs = e as ContextRefreshedEventArgs;
+
+            if (eventArgs == null) return;
+
+            var args = eventArgs;
+
+            if (args.Event != ContextEventArgs.ContextEvent.Refreshed) return;
+
+            var ctx = (IApplicationContext)sender;
+            var eventService = (IEventService)ctx.GetObject("EventService");
+            eventService.RegisterEvents();
+        }
+    }
+}
--- workflow-for-umbraco-6/Umbraco.Application/UmbracoWorkflowApplicationBase.cs
+++ workflow-for-umbraco-6/Umbraco.Application/UmbracoWorkflowApplicationBase.cs
@@ -1,40 +1,18 @@
 ﻿using System.Reflection;
 using Common.Logging;
-using FergusonMoriyam.Workflow.Interfaces.Application.Event;
 using FergusonMoriyam.Workflow.Umbraco.Application.Modules;
 using Microsoft.Web.Infrastructure.DynamicModuleHelper;
-using Spring.Context.Support;
-using umbraco.BusinessLogic;
-using umbraco.cms.presentation.Trees;
+using Umbraco.Core;
+using ApplicationEventHandler = Umbraco.Core.ApplicationEventHandler;
 
 namespace FergusonMoriyam.Workflow.Umbraco.Application
 {
-    public class UmbracoWorkflowApplicationBase : ApplicationBase
+    public class UmbracoWorkflowApplicationBase : ApplicationEventHandler 
     {
         private static readonly ILog Log = LogManager.GetLogger(MethodBase.GetCurrentMethod().DeclaringType);
-
+        
         private static bool _modulesRegistered;
-
-        public UmbracoWorkflowApplicationBase()
-        {
-            Log.Info(string.Format("Starting workflow for Umbraco {0}", Assembly.GetExecutingAssembly().GetName().Version));
-
-            var ctx = ContextRegistry.GetContext();
-            var eventService = (IEventService)ctx.GetObject("EventService");
-            
-
-            Log.Debug("Registering events");
-            eventService.RegisterEvents();
-
-            Log.Debug("Registering custom menu items");
-            BaseContentTree.BeforeNodeRender += BaseContentTreeBeforeNodeRender;
-        }
-
-        void BaseContentTreeBeforeNodeRender(ref XmlTree sender, ref XmlTreeNode node, System.EventArgs e)
-        {
-            Log.Debug(string.Format("Rendering Tree " + sender.GetType()));
-        }
-
+        
         public static void RegisterModules()
         {
             if (_modulesRegistered)
@@ -42,6 +20,12 @@ namespace FergusonMoriyam.Workflow.Umbraco.Application
 
             DynamicModuleUtility.RegisterModule(typeof(RegisterClientResourcesHttpModule));
             _modulesRegistered = true;
+        }
+
+        protected override void ApplicationStarting(UmbracoApplicationBase umbracoApplication, ApplicationContext applicationContext)
+        {
+            Log.Info(string.Format("Starting workflow for Umbraco {0}", Assembly.GetExecutingAssembly().GetName().Version));
+            base.ApplicationStarting(umbracoApplication, applicationContext);
         }
     }
 }
